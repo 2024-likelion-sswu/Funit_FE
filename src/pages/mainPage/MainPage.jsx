e04b7a2bc5190e42cfd7a5ad9e8998e476a44f13
nakyeong <yunna3425@naver.com>
2025-01-01 16:16:23 +0900

    [Feature] #1 - 스플래시 화면 구현

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import icon from '../../assets/img/character/splash.png'
+import icon from '../../assets/img/main/splash.png'
 
 const MainPage = () => {
     return (
         <div className='container splash-screen'>
             <img src={icon} alt="스플레시 화면 아이콘" />
+            <h1>펀잇</h1>
         </div>
     )
 }
