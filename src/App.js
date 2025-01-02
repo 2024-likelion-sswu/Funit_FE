a139e9e1985a71bbbf95f20fe11367f8de425e70
nakyeong <yunna3425@naver.com>
2025-01-02 14:59:37 +0900

    [Feature] #4 - 닉네임 생성 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/mainPage/MainPage'
+import NicknamePage from './pages/testCreatePage/NicknamePage'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-            <Route path='/' element={<Main />} />
+                <Route path='/' element={<Main />} />
+                <Route path='/nickname' element={<NicknamePage />} />
             </Routes>
         </BrowserRouter>
     )
