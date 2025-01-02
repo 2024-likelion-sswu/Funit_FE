be815bc3a788e1f59a0f7ff285edb84b59cfe6eb
yengeun <7613ez@gmail.com>
2025-01-02 17:56:22 +0900

    [Feature] #8 - 10점 페이지 완성

--- src/pages/testScorePage/Score1Page.jsx
+++ src/pages/testScorePage/Score1Page.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../../components/Button';
+import icon from '../../assets/img/character/mint.png'
 
 const Score1Page = () => {
+    const [score, setScore] = useState(10); 
+    const [nickname, setNickname] = useState('Guest');
     return (
         <div className='container score-container'>
-            <p className='input-ment'>닉네임을 입력하세요</p>
-            <div>
-                
-            </div>
+            <h2><span style={{color:"#FF7F71"}}>{score}</span>점</h2>
+            <h3>{nickname}님과 통하는 사이 !</h3>
+            <img src={icon} alt="10점" />
+            <div className='alert'>
+                <p>6점을 넘기면 친구에게 편지를 남길 수 있어요!</p>
             <Button title="편지 쓰러 가기"/>
+            </div>
         </div>
     )
 }
 
 export default Score1Page
+
+
+
