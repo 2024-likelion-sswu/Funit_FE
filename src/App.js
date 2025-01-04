5ea6e7d0747850f40ca237ba30bd9839b8eb5f21
nakyeong <yunna3425@naver.com>
2025-01-04 15:59:17 +0900

    [Feature] #16 - 편지 보내기 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Score1Page from './pages/testScorePage/Score1Page'
 import Score2Page from './pages/testScorePage/Score2Page'
 import Score3Page from './pages/testScorePage/Score3Page'
 import TestPage from './pages/testPage/TestPage'
+import LetterCreatePage from './pages/letterPage/LetterCreatePage'
 
 const App = () => {
     return (
@@ -29,6 +30,7 @@ const App = () => {
                 <Route path ='/score2' element={<Score2Page/>} />
                 <Route path ='/score3' element={<Score3Page/>} />
                 <Route path ='/test' element={<TestPage/>} />
+                <Route path ='/letterCreate' element={<LetterCreatePage/>} />
             </Routes>
         </BrowserRouter>
     )
