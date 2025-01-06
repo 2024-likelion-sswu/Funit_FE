9e5d628ed81a365c56eecdd7cb5cce673708c439
yengeun <7613ez@gmail.com>
2025-01-06 16:15:41 +0900

    [Feature] #24 - onclick 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({title}) => {
+const Button = ({title, onClick}) => {
     return (
-        <div className='button-container'>
+        <div className='button-container' onClick={onClick}>
             <p>{title}</p>
         </div>
     )
