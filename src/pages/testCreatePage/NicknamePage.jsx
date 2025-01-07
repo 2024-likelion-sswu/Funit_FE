872401e7374fc68e47d8bb990e316f4d0fea8d28
nakyeong <yunna3425@naver.com>
2025-01-07 15:32:18 +0900

    Feat: 닉네임 페이지에 비밀번호 input 추가

--- src/pages/testCreatePage/NicknamePage.jsx
+++ src/pages/testCreatePage/NicknamePage.jsx
@@ -6,9 +6,10 @@ const NicknamePage = () => {
         <div className='container nickname-container'>
             <p className='input-ment'>닉네임을 입력하세요</p>
             <div>
-                <input placeholder='닉네임' />
+                <input type='text' placeholder='닉네임' />
                 <button>중복확인</button>
             </div>
+            <input type='text' placeholder='비밀번호 입력' className='passsword-input'/>
             <Button title="시작하기"/>
         </div>
     )
