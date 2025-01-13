37dc6c3f5c4689cae571b4797800d771fce6ba43
nakyeong <yunna3425@naver.com>
2025-01-13 18:52:38 +0900

    Feat: 편지 트리와 편지 디테일 API 연결하는 중

--- src/pages/letterPage/LetterTreePage.jsx
+++ src/pages/letterPage/LetterTreePage.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import treeImage from '../../assets/img/letter/tree.svg'; 
 import envelopePink from '../../assets/img/letter/pinkletter.svg'; 
 import envelopeBlue from '../../assets/img/letter/blueletter.svg';
@@ -7,14 +6,15 @@ import envelopePurple from '../../assets/img/letter/purpleletter.svg';
 import envelopeYellow from '../../assets/img/letter/yellowletter.svg';
 import letterImg from '../../assets/img/letter/letterOpen.png';
 import iconPink from '../../assets/img/character/pink.png';
+import iconBlue from '../../assets/img/character/skyblue.png'
+import iconYellow from '../../assets/img/character/yelloweye.png'
+import axiosInstance from '../../apis/axiosInstance';
 
 const LetterTreePage = () => {
-  const navigate = useNavigate();
+  const nickname = localStorage.getItem('username');
 
-
-  const nickname = 'Guest';
-  const receivedLetters = 5;
-
+  const [receivedLetters, setReceivedLetters] = useState([]);
+  // const receivedLetters = 5;
 
   const envelopeImages = [
     envelopePink,
@@ -24,38 +24,75 @@ const LetterTreePage = () => {
     envelopePurple,
   ]; 
 
-
   // const handleLetterClick = (index) => {
   //   navigate('/letterRead', { state: { letterIndex: index } });
   // };
 
+  const getLetterId = async () => {
+    try {
+      const response = await axiosInstance.get('/api/letters/received');
+      console.log('편지 목록 id :', response);
+      setReceivedLetters(response.data);
+    } catch(error) {
+      console.log('편지 목록 id get 실패 : ', error);
+    }
+  }
+
+  const getLetterContent = async (id) => {
+    try {
+      setLoading(true);
+      const response = await axiosInstance.get(`/api/letters/${id}`);
+      console.log('편지 디테일 가져오기 성공 : ', response);
+      setLetterContent(response.data);
+      setColor(response.data.paperColor);
+      setOpenLetter(true);
+    } catch(error) {
+      console.log('편지 디테일 가져오기 실패 : ', error);
+    } finally {
+      setLoading(false);
+    }
+  }
   
   // 편지 내용 부분
   const [loading,setLoading] = useState(false);
   const [openLetter, setOpenLetter] = useState(false);
   const [selectedLetter, setSelectedLetter] = useState(null);
+  const [letterContent, setLetterContent] = useState(null);
+  const [color, setColor] = useState('red');
 
-  const letterOpen = (index) => {
-    setLoading(true);
-    setOpenLetter(false);
-
-    setTimeout(() => {
-      setLoading(false);
-      setOpenLetter(true);
-    }, 1000);
+  const letterOpen = (id) => {
+    setSelectedLetter(id);
+    getLetterContent(id);
   };
 
   const letterClose = () => {
     setLoading(false);
     setOpenLetter(false);
     setSelectedLetter(null);
+    setLetterContent(null);
   }
+  useEffect(() => {
+    getLetterId();
+  }, []);
+
+  const getPaperColor = (color) => {
+    switch (color) {
+      case 'red': 
+        return 'rgba(255, 127, 113, 0.3)';
+      case 'blue':
+        return 'rgba(179, 226, 225, 0.4)';
+      case 'yellow':
+        return 'rgba(255, 238, 177, 0.4)';
+      default:
+        return 'rgba(255, 127, 113, 0.3)';
+    }
+  };
 
   return (
     <div className="container letter-tree-container">
       <p className="letter-header">
         <strong>{nickname}</strong>님에게<br />
-        {receivedLetters}개의 편지가 도착했어요!
+        {receivedLetters.length}개의 편지가 도착했어요!
       </p>
       <div className="tree-container">
         <img src={treeImage} alt="트리" className="tree-image" />
@@ -81,12 +118,19 @@ const LetterTreePage = () => {
           )}
           {openLetter && (
             <div className='letter-container'>
-              <div className='letter'>
+              <div className='letter' style={{ backgroundColor: getPaperColor(color) }}>
                 <div className='letter-content'>
-                안녕, [닉네임]! <br /><br />요즘 어떻게 지내? <br />네가 좋아하는 테니스는 잘하고 있겠지?<br /><br /> 나는 요즘 열심히 공부 하면서 지내고 있어. 가끔 네가 생각날 때마다 예전에 함께했던 추억들이 떠오르더라. 그때 많이 웃고 즐거웠던 기억이 참 소중하게 느껴져.<br /><br /> 조만간 우리 꼭 만나서 맛있는 거 먹고 이야기 나누자! 나도 곧 테스트 만들 테니까 기대해! 😄
+                {letterContent.content}
                 </div>
                 <div className='letter-bottom'>
-                  <img src={iconPink} alt="편지지 아이콘" />
+                  <img src={
+                    color === 'red' ? iconPink :
+                    color === 'blue' ? iconBlue :
+                    color === 'yellow' ? iconYellow :
+                    iconPink
+                    } 
+                    alt="편지지 아이콘" 
+                  />
                 </div>
               </div>
               <button className='letter-close' onClick={letterClose}>
